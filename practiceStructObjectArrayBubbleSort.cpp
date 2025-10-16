3a84d4cf20c4cf00e651631a9779b25368bd75d8
Ramelito Martinez <ramelitomartinez@gmail.com>
2025-10-16 23:59:31 +0800

    added dec to bin converstion

--- practiceStructObjectArrayBubbleSort.cpp
+++ practiceStructObjectArrayBubbleSort.cpp
@@ -50,9 +50,71 @@ void runPracticeArrayObjects(){
         cout << i+1 << "\t" << Employees[i].name << "\t" << Employees[i].age << endl;
     }
 }
+
+bool isValidDecimal(string givenDecimal){
+    //step 1: loop through the givenDecimal
+    for(int i=0;i<givenDecimal.length();i++){
+        if(isalpha(givenDecimal[i])){
+            return false;
+        }
+    }
+    //step 2: convert the givenDecimal to integer
+    int givenInteger = stoi(givenDecimal);
+    //step 3: return false if value is less than 0
+    if(givenInteger < 0){
+        return false;
+    }
+    //step 4: return true if all goods
+    return true;
+}
+
+string convertDecimalToBinary(string givenDecimal){
+    //step 1: convert givenDecimal to int
+    int givenInteger = stoi(givenDecimal);
+    //step 2: let's determine the bitValue by
+    //increasing its value 2x every iteration as
+    //long as the bitValue is less than the givenInteger
+    int bitValue = 1;
+    while(bitValue<=givenInteger){
+        bitValue*=2;
+    }
+    //step 3: go down one bitValue so that givenInteger is still one bitValue higher
+    bitValue/=2;
+    //step 3: prepare binaryResult Variable
+    string binaryResult = "";
+    //step 4: iterate through bitValue until bitValue is 0
+    while(bitValue!=0){
+        //step 5: concatenate 1 to binaryResult if givenInteger is greater
+        //than or equal to bitValue
+        if(givenInteger >= bitValue){
+            binaryResult+= '1';
+            givenInteger-=bitValue;
+        }else{
+            //step 6: concatenate 0 if otherwise
+            binaryResult+= '0';
+        }
+        //step 7: divide bitValue by two every iteration
+        bitValue/=2;
+    }
+    //step 8: return binaryResult;
+    return binaryResult;
+}
 void runBinaryConversionLowToHigh(){
     string title = "Binary Conversion Low To High";
     cout << "Title: " << title << endl;
+    //step 1: get user input
+    string decimal;
+    cout << "Enter your decimal " << endl;
+    cin >> decimal;
+    //step 2: validate input
+    while(!isValidDecimal(decimal)){
+        cout << "That is not valid decimal or integer. Try again!" << endl;
+        cin >> decimal;
+    }
+    //step 3: do bin conversion
+    string binaryResult = convertDecimalToBinary(decimal);
+    //step 4: display binaryResult;
+    cout << "Binary Result " << binaryResult << endl;
 }
 
 void runBinaryConversionHighToLow(){
@@ -96,7 +158,6 @@ void acceptUserOption(){
             showOptions();
             break;
         case '3':
-            case '2':
             clearScreen();
             showDivider();
             runBinaryConversionHighToLow();
