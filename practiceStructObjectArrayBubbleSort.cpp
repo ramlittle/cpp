cdfa26e76e07dfb54f0bbf961414725c2e0a391b
ramlittle <ramelitomartinez@gmail.com>
2025-10-16 08:43:27 +0800

    added arr obj

--- practiceStructObjectArrayBubbleSort.cpp
+++ practiceStructObjectArrayBubbleSort.cpp
@@ -29,11 +29,40 @@ void clearScreen(){
 void runPracticeArrayObjects(){
     string title = "Practice Array Objects";
     cout << "Title: " << title << endl;
+    //step 1: get employee count
+    int employeeCount=0;
+    cout << "How many Employees would you like to record" << endl;
+    cin >> employeeCount;
+    //step 2: declare array obj
+    MyObject Employees[employeeCount];
+    //step 3: let user enter the details of employees in the array
+    cout << "Enter the details of employees" << endl;
+    for(int i =0;i<employeeCount;i++){
+        cout << "Entering details for employee " << i+1 << " : " << endl;
+        cout << "Name: " << endl;
+        cin >> Employees[i].name;
+        cout << "Age: " << endl;
+        cin >> Employees[i].age;
+    }
+    //step 4: display the list that was entered in table form
+    cout << "Item #" << "\t" << "Name" << "\t" << "Age" << endl;
+    for(int i=0;i<employeeCount;i++){
+        cout << i+1 << "\t" << Employees[i].name << "\t" << Employees[i].age << endl;
+    }
+}
+void runBinaryConversionLowToHigh(){
+    string title = "Binary Conversion Low To High";
+    cout << "Title: " << title << endl;
+}
+
+void runBinaryConversionHighToLow(){
+    string title = "Binary Conversion High To Low";
+    cout << "Title: " << title << endl;
 }
 void showOptions(){
     cout << "What would you like to do?"<< endl;
     cout << "1: Practice Array Objects" << endl;
-    cout << "2: Enter a binary, convert to octal, sort the result lowest to highest, then display the decimal result" << endl;
+    cout << "2: Enter a decimal, system will convert to binary, convert to octal, sort the result lowest to highest, then display the decimal result" << endl;
     cout << "3: Enter a decimal, convert to binary, convert to octal, sort result highest to lowest, then display the result in decimal" << endl;
     cout << "0: Exit" << endl;
 }
@@ -56,6 +85,21 @@ void acceptUserOption(){
             clearScreen();
             showDivider();
             runPracticeArrayObjects();
+            showDivider();
+            showOptions();
+            break;
+        case '2':
+            clearScreen();
+            showDivider();
+            runBinaryConversionLowToHigh();
+            showDivider();
+            showOptions();
+            break;
+        case '3':
+            case '2':
+            clearScreen();
+            showDivider();
+            runBinaryConversionHighToLow();
             showDivider();
             showOptions();
             break;
